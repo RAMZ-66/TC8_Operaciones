7dd569a1783c960261ebf666d4c0a40c707eaec0
RAMZ-66 <axelramz_@hotmail.com>
2021-05-25 13:01:09 -0600

    Se desarrolló el programa por completo

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace TC8_Operaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Console.WriteLine("Introduce el valor a:");
+                // Transformar a float lo que se introduzca en la consola
+                float a = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Introduce el valor b:");
+                float b = float.Parse(Console.ReadLine());
+
+                // float a = 10f;
+                // float b = 2f;
+
+                float result = a / b;
+                Console.WriteLine($"{a} / {b} = {result} ");
+
+                using(StreamWriter sw = new StreamWriter("operacion.tx"))
+                {
+                    sw.WriteLine($"{a} / {b} = {result}");
+                }
+            }
+            catch(SystemException)
+            {
+                // Si ocurre una excepcion en el try, se ejecuta el código en este catch
+                Console.WriteLine("Error: Porfavor introduce números válidos");
+                throw;
+            }
+
+
+        }
+    }
+}
